92287f98184fa2fcd21c16b0b822b96facab8958
mitchi <darkshooter@gmail.com>
2018-05-30 12:15:33 -0400

    Ajout de commentaires sur le Edge Cover

--- src/main/scala/org/apache/spark/Algorithm.scala
+++ src/main/scala/org/apache/spark/Algorithm.scala
@@ -1,13 +1,27 @@
 package org.apache.spark
-
 import ca.lif.sparklauncher.app.CustomLogger
 import org.apache.spark.Models.Hyperedge
 import org.apache.spark.rdd.RDD
 
 import scala.collection.mutable.ArrayBuffer
 
+/*This is the Edge Cover hypergraph solver
+
+We represent a T-way testing problem with the Edge Cover
+
+ * Input is a file of n lines. A line is a hyperedge
+  * A hyperedge is a test that covers a number (m) of values
+  * We choose a hyperedge that tests the most values. We then remove these tested values from other hyperedges
+  * We end the algorithm when everything has been tested.
+  *
+  * This algorithm is greedy, but there is some randomness.
+  * By picking a random edge when others are equal, we get different results.
+  * Spark's random shuffle already gives us a built-in random. By running the algorithm multiple times
+  * on the same data, the hyperedges get shuffled in other places. Which means that when doing the a < b comparison,
+  * we don't always have the same hyperedge in a and b.
+  * */
+
 object Algorithm {
-  //Greedy algorithm here
   def greedy_algorithm(sc: SparkContext, rdd: RDD[Hyperedge]): ArrayBuffer[Long] = {
 
     var currentRDD = rdd
@@ -17,31 +31,31 @@ object Algorithm {
     def loop(): Unit = {
       while (true) {
 
+        //We destroy the RDD lineage at every iteration.
         currentRDD.localCheckpoint()
 
         CustomLogger.logger.info(s"ITERATION NUMBER : $counter")
         counter += 1
 
-        //Condition de fin, le RDD est vide
+        //Exit condition, the RDD is empty
         if (currentRDD.isEmpty()) return
 
         //LoggerForTest.logger.fine("TEST EMPTY FINISHED")
 
-        //Trouver l'edge avec le meilleur count
+        //Find the hyperedge with the best count
+        //Here, random shuffling gives us built-in randomness. But we could do more (ACTION)
         val smallEdge = currentRDD.reduce((a, b) => {
           if (a.listVertices.size > b.listVertices.size) a else b
         })
 
-        //LoggerForTest.logger.fine("SMALLER EDGE FOUND")
-
         //Mettre notre hyperedge choisie en broadcast variable (optimisation réseau)
+        //Put the hyperedge in a broadcast variable
         val smallEdgeBC = sc.broadcast(smallEdge)
 
-        //Garder l'edge dans le log
+        //Keep it in the log
         logEdgesChosen.append(smallEdge.edgeID)
 
-        //LoggerForTest.logger.fine("SMALLER EDGE BROADCAST")
-
+        //Here we apply a flatmap to remove elements from all sets using this smalledge (Transformation)
         currentRDD = currentRDD.flatMap(he => {
           var result = Seq[Hyperedge]()
           val updatedListVertices = he.listVertices -- smallEdgeBC.value.listVertices
@@ -52,9 +66,6 @@ object Algorithm {
           result
         })
 
-        // currentRDD.take(1)
-
-        //LoggerForTest.logger.info("END OF ITERATION")
       }
     }
 
